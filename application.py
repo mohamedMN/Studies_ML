3fbdbcbbad22195eb05b20a9dd8d831f3a0c3211
Mohammed El Mouden <mohamed55mouden@gmail.com>
2025-01-05 13:38:25 +0100

    deploimeent config

--- application.py
+++ application.py
@@ -1,19 +1,18 @@
 from doctest import debug
 from email.mime import application
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline import predict_pipeline
-from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application = Flask(__name__)
 
 
-app = application
+app=application
 
-# routes of home
-
+## routes of home
 
 @app.route("/")
 def index():
@@ -22,7 +21,7 @@ def index():
 
 @app.route("/predictdata", methods={"GET", "POST"})
 def predict_datapoint():
-    if request.method == "GET":
+    if request.method=="GET":
         return render_template("home.html")
     else:
         data = CustomData(
@@ -36,13 +35,14 @@ def predict_datapoint():
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
         )
-        pred_df = data.get_data_as_data_frame()
+        pred_df=data.get_data_as_data_frame()
         print(pred_df)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
+        predict_pipeline=PredictPipeline()
+        results=predict_pipeline.predict(pred_df)
         return render_template('home.html', results=results[0])
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
